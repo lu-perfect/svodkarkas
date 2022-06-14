eca9d3b729318d6793e885e9d08ebb84796485c3
gfnrf2 <gfnrf2@gmail.com>
2022-06-14 10:57:59 +0300

    fix redux folder

--- src/hooks/redux.ts
+++
@@ -1,4 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-
-export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
